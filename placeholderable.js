b874e0c4bd4c20f5828fb2fa960f2352a1c613ed
TETSUAKI BABA <baba@bolon.local>
2024-02-10 23:15:19 +0900

    update

--- placeholderable.js
+++ placeholderable.js
@@ -25,7 +25,7 @@ function placeholderableGenerateImage(element) {
     const width = parseInt(params.width || defaultWidth);
     const height = parseInt(params.height || defaultHeight);
     const text = decodeURIComponent(params.text || `${width}x${height}`);
-    const bootstrap_icon = decodeURIComponent(params.bicon || `${defaultBootstrapIcon}`);
+    const icon = decodeURIComponent(params.icon || `${defaultBootstrapIcon}`);
     const bgcolorOptions = decodeURIComponent(params.bgcolors || defaultBgColors.join('#')).split('#').filter(c => c);
     const textColorOptions = decodeURIComponent(params.colors || defaultTextColors.join('#')).split('#').filter(c => c);
 
@@ -45,23 +45,19 @@ function placeholderableGenerateImage(element) {
     ctx.fillStyle = bgcolor;
     ctx.fillRect(0, 0, width, height);
 
-    // フォントサイズとフォントスタイルを設定
-    const fontSize = Math.max(Math.min(width / (text.length < 10 ? 10 : text.length) * 2, height / 2), 10);
-
+    let fontSize;
     ctx.fillStyle = textColor;
 
     // テキストを中央に描画
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    if (bootstrap_icon != '') {
-        // document.fonts.load('10pt Bootstrap-Icons').then(function () {
+    if (icon != '') {
+        fontSize = Math.min(width, height) * 0.7;
         ctx.font = `${fontSize}px Bootstrap-Icons`;
-        // ctx.font = `48px Bootstrap-Icons`;
-        ctx.fillText(String.fromCharCode(`0x${bootstrap_icon}`), width / 2, height / 2);
-
-        // });
+        ctx.fillText(String.fromCharCode(`0x${icon}`), width / 2, height / 2);
     }
     else {
+        fontSize = Math.max(Math.min(width / (text.length < 10 ? 10 : text.length) * 2, height / 2), 10);
         ctx.font = `${fontSize}px Arial`;
         ctx.fillText(text, width / 2, height / 2);
     }
@@ -80,56 +76,6 @@ function placeholderable() {
 }
 
 // DOMが読み込まれた後にplaceholderable関数を呼び出す
-document.addEventListener('DOMContentLoaded', placeholderable);
-
-
-
-// // 画像生成関数
-// function placeholderableGenerateImage(element) {
-//     // デフォルト値の設定
-//     const defaultWidth = 200;
-//     const defaultHeight = 100;
-//     const defaultText = `${defaultWidth}x${defaultHeight}`; // デフォルトのテキストオプション
-//     const defaultBgColors = ['#F2529D', '#A99CD9', '#9080F2', '#05F2F2', '#F2CB05']; // デフォルトの背景色オプション
-//     const defaultTextColors = ['EEEEEE']; // デフォルトのテキスト色オプション
-
-//     // 属性があればその値を使用し、なければデフォルト値を使用
-//     const width = parseInt(element.getAttribute('data-ph-width') || defaultWidth, 10);
-//     const height = parseInt(element.getAttribute('data-ph-height') || defaultHeight, 10);
-//     const text = element.getAttribute('data-ph-text') || defaultText;
-//     const bgcolorOptions = (element.getAttribute('data-ph-bgcolors') || defaultBgColors.join('#')).split('#').filter(c => c);
-//     const textColorOptions = (element.getAttribute('data-ph-colors') || defaultTextColors.join('#')).split('#').filter(c => c);
-
-//     // 背景色とテキスト色をランダムに選択
-//     const bgcolor = `#${bgcolorOptions[Math.floor(Math.random() * bgcolorOptions.length)]}`;
-//     const textColor = `#${textColorOptions[Math.floor(Math.random() * textColorOptions.length)]} `;
-
-//     // Canvasを作成
-//     const canvas = document.createElement('canvas');
-//     const ctx = canvas.getContext('2d');
-//     canvas.width = width;
-//     canvas.height = height;
-
-//     // 背景色を設定
-//     ctx.fillStyle = bgcolor;
-//     ctx.fillRect(0, 0, width, height);
-
-//     // フォントサイズを適切な値に設定
-//     const fontSize = Math.max(Math.min(width / (text.length < 10 ? 10 : text.length) * 2, height / 2), 10); // サイズと高さを考慮
-//     ctx.font = `${fontSize}px Arial`;
-//     ctx.fillStyle = textColor;
-
-//     // テキストを中心に描画
-//     ctx.textAlign = 'center'; // テキストを中央揃えに
-//     ctx.textBaseline = 'middle'; // テキストのベースラインを中央に
-//     ctx.fillText(text, width / 2, height / 2);
-
-//     // imgタグのsrcに設定
-//     element.src = canvas.toDataURL('image/png');
-// }
-
-// // 画像生成関数を呼び出す
-// function placeholderable() {
-//     const elements = document.querySelectorAll('img[data-ph]');
-//     elements.forEach(placeholderableGenerateImage);
-// }
+window.addEventListener('load', function () {
+    placeholderable();
+});
